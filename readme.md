b2dc328a49eb869a6ff479ba17ddd2c49f370e6c
Insidexa <nostresss77@gmail.com>
2021-01-08 09:48:55 +0200

    init

---
+++ readme.md
@@ -0,0 +1,13 @@
+Define env before start app  
+
+ - `APP_PORT` - application port
+ - `DB_USER` - db username
+ - `DB_HOST` - db host / ip
+ - `DB_NAME` - db name
+ - `DB_PASSWORD` - db password
+ - `DB_PORT` - db port
+
+
+ TODO:
+  - implement Dockerfile
+  - implement .dockerignore
---
+++ src/index.js
@@ -0,0 +1,33 @@
+const { Client } = require('pg')
+const express = require('express')
+
+const app = express()
+const port = process.env.APP_PORT ? parseInt(process.env.APP_PORT, 10) : 4001;
+const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5433,
+})
+client.connect()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+  
+app.get('/healthcheck/db', (req, res) => {
+    
+    client.query('SELECT $1::text as message', ['[db]: Hello world!'])
+        .then(({ rows }) => {
+            return rows[0].message;
+        })
+        .then(msg => {
+            res.send(msg)
+        })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at :${port}`)
+})
+  
