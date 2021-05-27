afdd38d92fd067e9f38089c493f0410045689545
ildo <81921033+bey-dmitry@users.noreply.github.com>
2021-05-27 21:56:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ client.connect()
 
 app.get('/', (req, res) => {
     console.log('route: /')
-    res.send('Hello World After 0 days!')
+    res.send('Hello World TEST01!')
 })
   
 app.get('/healthcheck/db', (req, res) => {
